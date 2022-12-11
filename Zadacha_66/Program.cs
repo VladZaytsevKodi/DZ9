7183ed716ca14ec3bd55d927508db5882fcf9200
Vlad <vlad_zaycev_1984@mail.ru>
2022-12-11 20:10:15 +0300

    3 задачи

---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.WriteLine("Ввести число M(начало проежутка) ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Ввести число N(конец проежутка) ");
+int n = int.Parse(Console.ReadLine()!);
+
+int SumNumbers(int m, int n)
+{
+    if (m > n) 
+    {
+     return n + SumNumbers(m, n + 1);
+    }
+    if (m < n)
+    {
+      return n + SumNumbers(m, n - 1);  
+    }
+    if (m == n)
+    {
+      return 0;  
+    }
+    return SumNumbers(m, n);
+}
+Console.WriteLine($"результат {SumNumbers(m, n)}");
---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine()!);
+
+int AckermannFunction (int m, int n)
+{
+    if (m == 0)
+    { 
+        return n + 1;
+    }
+    if (n == 0)
+    {
+     return AckermannFunction(m - 1, 1);
+    }
+    if (m > 0 && n > 0) 
+    {
+    return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+    return AckermannFunction(m, n);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({m},{n}) = {AckermannFunction(m, n)}");
